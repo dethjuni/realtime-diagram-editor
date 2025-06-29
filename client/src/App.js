d111fae8bdd29b5028e7f518b1649e545d163eaa
DJ-GB3PRO-KD72G\dethjuni <dethjuni@hotmail.com>
2025-06-29 17:42:23 +0900

    3. 클라이언트 기본 UI 및 실시간 사각형 동기화 기능 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { socket } from "./socket";
 
 function App() {
+  const [shapes, setShapes] = useState([]);
+
+  useEffect(() => {
+    socket.on("init", (data) => setShapes(data));
+    socket.on("update", (data) => setShapes(data));
+    return () => {
+      socket.off("init");
+      socket.off("update");
+    };
+  }, []);
+
+  const addRectangle = () => {
+    const newShapes = [...shapes, { id: Date.now(), type: "rect", x: 100, y: 100 }];
+    setShapes(newShapes);
+    socket.emit("update", newShapes);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={addRectangle}>사각형 추가</button>
+      <svg width="800" height="600" style={{ border: "1px solid black" }}>
+        {shapes.map((shape) =>
+          shape.type === "rect" ? (
+            <rect key={shape.id} x={shape.x} y={shape.y} width="100" height="50" fill="skyblue" />
+          ) : null
+        )}
+      </svg>
     </div>
   );
 }
