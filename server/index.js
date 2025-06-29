3146710141ac0973c28d3259abacbed4d8cd6ae5
DJ-GB3PRO-KD72G\dethjuni <dethjuni@hotmail.com>
2025-06-29 18:43:02 +0900

    서버 다이어그램 상태 JSON 파일 저장 기능 추가

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
 app.use(cors());
@@ -12,7 +13,13 @@ const io = new Server(server, {
   },
 });
 
+const SAVE_FILE = "diagram-state.json";
 let diagramState = []; // JSON 형식으로 메모리에 상태 저장
+
+// 서버 시작 시 이전 상태 로드 (있으면)
+if (fs.existsSync(SAVE_FILE)) {
+  diagramState = JSON.parse(fs.readFileSync(SAVE_FILE, "utf-8"));
+}
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
@@ -20,9 +27,11 @@ io.on("connection", (socket) => {
   // 접속 시 현재 상태 전송
   socket.emit("init", diagramState);
 
+  // 업데이트 수신
   socket.on("update", (data) => {
     diagramState = data;
     socket.broadcast.emit("update", diagramState);
+    fs.writeFileSync(SAVE_FILE, JSON.stringify(diagramState));
   });
 
   socket.on("disconnect", () => {
